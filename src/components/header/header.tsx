8df68c0363282dc58ece94a82928cc009d7c2818
Javier <alexanderjmzc@gmail.com>
2024-05-04 21:06:55 -0500

    Header component almost done

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Logo from '../../assets/img/logo.png';
 
 export const Header = () => {
-  return (
-    <div className='bg-secondary'>
-        Hola
-    </div>
-  )
+    return (
+        <div className='bg-primary absolute w-screen flex justify-around'>
+            <div className='flex gap-x-3 items-center'>
+                <img className='w-20 h-20' src={Logo} alt="Refugees logo" />
+                <h1 className='text-white'>Refugees</h1>
+            </div>
+
+            <div className='flex gap-x-4 items-center text-white'>
+                <a href="#about">About</a>
+                <a href="#refugees">Refugees</a>
+            </div>
+        </div>
+    )
 }
