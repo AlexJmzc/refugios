62d238c0532bebf0d6ce4ee4f66af58525709e06
Javier <alexanderjmzc@gmail.com>
2024-05-04 20:43:05 -0500

    Interface and data

---
+++ src/assets/data/refugees.tsx
@@ -0,0 +1,15 @@
+import { Refugee } from '../interfaces/refugee';
+
+export const refugeesData: Refugee[] = [
+    {
+        id: 1,
+        name: "",
+        web: "",
+        country: "",
+        state: "",
+        city: "New York",
+        direction: "",
+        phone: "",
+        img: ""
+    }
+]
---
+++ src/assets/interfaces/refugee.tsx
@@ -0,0 +1,11 @@
+export interface Refugee {
+    id: number,
+    name: string,
+    web: string,
+    country: string,
+    state: string,
+    city: string,
+    direction: string,
+    phone: string,
+    img: string
+}
