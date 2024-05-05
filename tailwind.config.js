06b826e60fcf434e9a90413125184ecca812880d
Javier <alexanderjmzc@gmail.com>
2024-05-04 20:42:35 -0500

    Tailwind install

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#264653",
+        secondary: "#2A9D8F",
+        tertiary: "#E9C46A",
+        quaternary: "#F4A261",
+        fifth: '#E76F51'
+      },
+    },
+  },
+  plugins: [],
+}
+
