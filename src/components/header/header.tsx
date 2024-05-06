5e93fd31c9f03151ef0739a421f456d97c4f67f5
Javier <alexanderjmzc@gmail.com>
2024-05-05 20:31:39 -0500

    Language change

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,15 +2,14 @@ import Logo from '../../assets/img/logo.png';
 
 export const Header = () => {
     return (
-        <div className='bg-primary absolute w-screen flex justify-around'>
+        <div className='bg-primary w-screen flex justify-around'>
             <div className='flex gap-x-3 items-center'>
                 <img className='w-20 h-20' src={Logo} alt="Refugees logo" />
-                <h1 className='text-white'>Refugees</h1>
             </div>
 
-            <div className='flex gap-x-4 items-center text-white'>
-                <a href="#about">About</a>
-                <a href="#refugees">Refugees</a>
+            <div className='flex gap-x-12 items-center text-white'>
+                <a href="#about">Inicio</a>
+                <a href="#refugees">Refugios</a>
             </div>
         </div>
     )
