41a563eee952b88d323a9073aa250adc078fde45
Javier <alexanderjmzc@gmail.com>
2024-05-06 16:23:02 -0500

    Flags file and images

---
+++ src/assets/flags/flags.tsx
@@ -0,0 +1,63 @@
+import AntiguaBarbuda from './img/AntiguaBarbuda.png';
+import Argentina from './img/Argentina.png';
+import Bahamas from './img/Bahamas.png';
+import Barbados from './img/Barbados.png';
+import Belice from './img/Belice.png';
+import Bolivia from './img/Bolivia.png';
+import Brasil from './img/Brazil.png';
+import Chile from './img/Chile.png';
+import Colombia from './img/Colombia.png';
+import CostaRica from './img/CostaRica.png';
+import Cuba from './img/Cuba.png';
+import Dominica from './img/Dominica.png';
+import DominicanRepublic from './img/DominicanRepublic.png';
+import Ecuador from './img/Ecuador.png';
+import ElSalvador from './img/ElSalvador.png';
+import Granada from './img/Granada.png';
+import Guatemala from './img/Guatemala.png';
+import Guyana from './img/Guyana.png';
+import Haiti from './img/Haiti.png';
+import Honduras from './img/Honduras.png';
+import Jamaica from './img/Jamaica.png';
+import Mexico from './img/Mexico.png';
+import Nicaragua from './img/Nicaragua.png';
+import Panama from './img/Panama.png';
+import Paraguay from './img/Paraguay.png';
+import Peru from './img/Peru.png';
+import SantaLucia from './img/SantaLucia.png';
+import TrinidadTobago from './img/TrinidadTobago.png';
+import Uruguay from './img/Uruguay.png';
+import Venezuela from './img/Uruguay.png';
+
+export const flags = [
+    { name: "ANTIGUA Y BARBUDA", url: AntiguaBarbuda },
+    { name: "ARGENTINA", url: Argentina },
+    { name: "BAHAMAS", url: Bahamas },
+    { name: "BARBADOS", url: Barbados },
+    { name: "BELICE", url: Belice },
+    { name: "BOLIVIA", url: Bolivia },
+    { name: "BRASIL", url: Brasil },
+    { name: "CHILE", url: Chile },
+    { name: "COLOMBIA", url: Colombia },
+    { name: "COSTA RICA", url: CostaRica },
+    { name: "CUBA", url: Cuba },
+    { name: "DOMINICA", url: Dominica },
+    { name: "REPÚBLICA DOMINICANA", url: DominicanRepublic },
+    { name: "ECUADOR", url: Ecuador },
+    { name: "EL SALVADOR", url: ElSalvador },
+    { name: "GRANADA", url: Granada },
+    { name: "GUATEMALA", url: Guatemala },
+    { name: "GUYANA", url: Guyana },
+    { name: "HAITÍ", url: Haiti },
+    { name: "HONDURAS", url: Honduras },
+    { name: "JAMAICA", url: Jamaica },
+    { name: "MÉXICO", url: Mexico },
+    { name: "NICARAGUA", url: Nicaragua },
+    { name: "PANAMÁ", url: Panama },
+    { name: "PARAGUAY", url: Paraguay },
+    { name: "PERÚ", url: Peru },
+    { name: "SANTA LUCÍA", url: SantaLucia },
+    { name: "TRINIDAD Y TOBAGO", url: TrinidadTobago },
+    { name: "URUGUAY", url: Uruguay },
+    { name: "VENEZUELA", url: Venezuela }
+]
