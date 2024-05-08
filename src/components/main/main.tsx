35718ca588081bea30cf156cda40d363d3738a48
Javier <alexanderjmzc@gmail.com>
2024-05-08 10:23:42 -0500

    Name

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import { Header } from '../header/header'
-import { Container } from '../container/container'
+import { Header } from '../header/header';
+import { Container } from '../container/container';
 
 export const Main = () => {
   return (
