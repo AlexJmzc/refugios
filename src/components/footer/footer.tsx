fdc102f27fcb76e36ea6b52b7a9c3f410a7f009c
Javier <alexanderjmzc@gmail.com>
2024-05-12 20:49:16 -0500

    Redistribution

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,4 +1,4 @@
-import FooterImage from '../../assets/img/footerImage.png';
+import FooterImage from '../../assets/img/footerImage.jpg';
 
 export const Footer = () => {
 
@@ -7,23 +7,23 @@ export const Footer = () => {
   }
 
   return (
-    <div className='w-screen bg-gray-300 py-4' id='help'>
-      <div className='bg-gradient-to-r from-secondary via-yellow-500/85 grid grid-cols-2 rounded-2xl mx-7 py-6 gap-y-3'>
+    <div className='w-screen bg-gray-200 py-4' id='help'>
+      <div className='bg-gradient-to-r from-red-200 via-blue-300 to-green-300 grid grid-cols-2 rounded-2xl mx-7 py-6 gap-y-3'>
         <div className='flex justify-center items-center'>
-          <img className='' src={FooterImage} alt="Help" />
+          <img className='w-2/3 rounded-full' src={FooterImage} alt="Help" />
         </div>
 
         <div className='flex flex-col items-center gap-y-5'>
-          <h2 className='font-bold text-lg text-white'>
+          <h2 className='font-bold text-lg'>
             ¿Cómo puedes ayudar?
           </h2>
 
-          <h2 className="text-white">
+          <h2 className="font-medium">
             Puedes enviar la información sobre algún refugio que no se encuentre en esta web
             a nuestras redes sociales con este formato:
           </h2>
 
-          <div className='bg-secondary grid grid-cols-1 gap-y-4 justify-items-center p-6 w-3/5 rounded-3xl text-white'>
+          <div className='bg-white grid grid-cols-1 gap-y-4 justify-items-center p-6 w-3/5 rounded-3xl font-semibold'>
             <div className=' flex flex-col items-center w-auto'>
               <h2>Página web</h2>
               <h2>Nombre del refugio</h2>
