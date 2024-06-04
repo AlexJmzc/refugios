85efd534aed882f6d16bb233ed698d4d899510a5
Javier <alexanderjmzc@gmail.com>
2024-06-03 20:44:29 -0500

    Language structure

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -2,8 +2,11 @@ import { Header } from '../header/header';
 import { Container } from '../container/container';
 import { Information } from '../information/information';
 import { Footer } from '../footer/footer';
+import { useState } from 'react';
 
 export const Main = () => {
+  const [language, setLanguage] = useState('Spanish');
+
   return (
     <>
         <Header/>
