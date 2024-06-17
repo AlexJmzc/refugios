6a4627f132fe7056eb8359bd1227141c60fba865
Javier <alexanderjmzc@gmail.com>
2024-06-16 19:56:24 -0500

    Logo change

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { englishData, spanishData } from './headerData';
 import { Switch } from '../switch/switch';
 
+import logo from '../../assets/img/logo.png';
+
 interface LanguageProps {
     language: string,
     setLanguage: (language: string) => void
@@ -40,8 +42,10 @@ export const Header = ({ language, setLanguage }: LanguageProps) => {
     return (
         <div className='bg-gray-200 w-screen hsm:pb-4 sm:pb-0 flex justify-between xsm:px-12 hsm:px-6 sticky top-0 z-50 pt-3'>
             <div className='flex items-center gap-x-2 justify-center cursor-pointer' onClick={(e) => { e.preventDefault(); scrollToSection('home'); }}>
-                <span className="md:w-16 md:h-14 xsm:w-10 xsm:h-8 icon-[streamline--pets-allowed]"></span>
-                <h2 className='font-main md:text-2xl xsm:text-base xsm:block hsm:hidden'>HOMEFORPAWS</h2>
+                <img src={logo}
+                 className='md:w-12 md:h-12 xsm:w-10 xsm:h-10'
+                 alt="Logo" />
+                <h2 className='font-main text-violet-800 md:text-2xl xsm:text-base xsm:block hsm:hidden'>HOMEFORPAWS</h2>
             </div>
 
             <div className='flex xsm:gap-x-12 hsm:gap-x-4 items-center font-medium xsm:text-base hsm:text-sm'>
